2b054128544f2677ac5f7cc9e1d167241def0a91
8000thCube <125620224+8000thCube@users.noreply.github.com>
2023-12-02 22:00:56 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,41 @@ fn main(){
 	test02();
 }fn test00(){
 	use ai::GeluMultilayerPerceptron;
-	let (input,mut testnetwork,target)=([0.0,0.0, 0.0,1.0, 1.0,0.0, 1.0,1.0, 0.5,0.5],GeluMultilayerPerceptron::new(2,10,2,1),[0.0,1.0,1.0,0.0,0.5]);
-	//println!("{}",testnetwork.train_rprop(&input,0.5,1.2,1000,&target).sqrt());
-	//println!("{}",testnetwork.train(&input,1000,0.5,0.01,&target).sqrt());
-	println!("{}",testnetwork.train_adamw(0.9,0.999,&input,1000,0.001,&target,0.01).sqrt());
+	let (input,mut testnetwork,target)=([0.0,0.0, 0.0,1.0, 1.0,0.0, 1.0,1.0, 0.5,0.5],GeluMultilayerPerceptron::new(true,2,10,2,1),[0.0,1.0,1.0,0.0,0.5]);
+	testnetwork.train(None,&input,1000,0.5,0.01,&target);
 	input.chunks_exact(2).for_each(|input|println!("{}",testnetwork.process(input)[0]));
 	println!("{}",testnetwork.process(&[0.75,0.75])[0]);
 }fn test01(){
 	use ai::SLT;
-	let (input,mut testnetwork,target)=("000001010011100101110111".as_bytes(),SLT::new(3,4,2,10,1),[0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,0.0,1.0, 0.0,1.0,1.0, 0.0,1.0,1.0, 0.0,1.0,0.0, 0.0,1.0,0.0]);
-	println!("{}",testnetwork.train(input,10000,0.5,0.0001,&target).sqrt());
+	let (input,mut testnetwork,target)=("000001010011100101110111".as_bytes(),SLT::new(3,2,10,1,256),[0.0,0.0,0.0, 0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,0.0,1.0, 0.0,1.0,1.0, 0.0,1.0,1.0, 0.0,1.0,0.0, 0.0,1.0,0.0]);
+	testnetwork.train(None,input.iter().map(|&x|x as usize),10000,0.5,0.001,&target);
 	input.chunks_exact(3).for_each(|input|{
-		testnetwork.process(input).iter().for_each(|x|print!("{} ",x));
+		testnetwork.process(input.iter().map(|&x|x as usize)).iter().for_each(|x|print!("{} ",x));
 		println!();
 	});
-}fn test02(){
-	use ai::GPT;
+}fn test02(){//ln(256)=5.545177 //lg(e)=1.442695
+	use ai::{GPT,NeuralNetwork,rseed,rsize};//h: 2.19
 	use save::Save;
 	use std::fs;
-	let (batchdimension,contextdimension)=(10,100);
-	let (batcharea,data,mut testnetwork)=(batchdimension*contextdimension,fs::read_to_string("dataset.txt").unwrap(),GPT::load("test save spot a").unwrap_or_else(|_|GPT::load("test save spot b").unwrap_or_else(|_|GPT::new(contextdimension,4,16,4,256))));
+	let (batchdimension,contextdimension,lossexamplecount,mut n,mut seed)=(10,100,10,0,rseed());
+	let (data,exampledimension,mut derivatives,mut testnetwork)=(fs::read_to_string("dataset.txt").unwrap(),contextdimension+1,NeuralNetwork::default(),GPT::load("test save spot a2").unwrap_or_else(|_|GPT::new(true,4,4,16,256,256)));
 	let (data,eval)=data.as_bytes().split_at(data.len()*9/10);
-	let (databound,evalbound)=(data.len().saturating_sub(batcharea),eval.len().saturating_sub(batcharea));
-	(0..10000).for_each(|n|{
-		let k=ai::rsize(databound,testnetwork.seed());
-		//testnetwork.train(&data[k..batcharea+k],0.9,1.0);
-		//testnetwork.train_rprop(&data[k..batcharea+k],0.5,1.2);
-		testnetwork.train_adamw(0.9,0.999,&data[k..batcharea+k],n,0.001,0.01);
-		if n%10==0{
-			let k=ai::rsize(evalbound,testnetwork.seed());
-			println!("iteration: {}",n);
-			println!("loss: {}",testnetwork.evaluate(&eval[k..batcharea+k]));
-			println!("sample: {}",testnetwork.sample_string());
-			testnetwork.save("test save spot a").unwrap();//TODO find a better way to avoid accidentally corrupting the save when terminating the program
-			testnetwork.save("test save spot b").unwrap();
-		}
+	let (databound,evalbound)=(data.len().checked_sub(exampledimension).unwrap(),eval.len().checked_sub(exampledimension).unwrap());
+	(0..10000).for_each(|_|{
+		testnetwork.train(batchdimension,exampledimension,Some(&mut derivatives),|network,networkderivatives|{
+			network.adamw(0.9,0.999,networkderivatives,0.00001,n,0.01);
+			//network.rprop(networkderivatives,0.8,1.25);
+			n+=1;
+		},1.0,(0..batchdimension*10).flat_map(move|_|{
+			let k=rsize(databound,&mut seed);
+			data[k..exampledimension+k].iter().map(|&x|x as usize)
+		}));
+		println!("iteration: {}",n);
+		println!("loss: {}",(0..lossexamplecount).map(|_|{
+			let k=rsize(evalbound,&mut seed);
+			testnetwork.evaluate(1.0,eval[k..exampledimension+k].iter().map(|&x|x as usize))
+		}).sum::<f32>()/lossexamplecount as f32);
+		println!("sample:\n{}",testnetwork.sample_string(contextdimension,contextdimension*2,1.0));
+		testnetwork.save("test save spot a2").unwrap();
 	});
 }
